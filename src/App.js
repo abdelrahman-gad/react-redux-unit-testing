4cd57d34578a0c8030ed0d6cd28f21976353f15c
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-09 04:38:43 +0200

    basic setup of libs and packages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './app.scss';
+
+
+
+class App extends Component {
+
+  render() {
+   
+
+    return (
+      <div className="App" >
+        <Header />
+       
+         
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ src/component/header/spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from './index';
+
+
+describe('Header Component', () => {
+
+    it('Should render without errors', () => {
+      
+        expect(1).toBe(1);
+    });
+
+});
